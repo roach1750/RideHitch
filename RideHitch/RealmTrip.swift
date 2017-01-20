da4b097a05831868e47c8975e3b5b86001bf5d58
Andrew Roach <roach1750@gmail.com>
2017-01-20 07:38:58 -0500

    added realm trips

---
+++ RideHitch/RealmTrip.swift
@@ -0,0 +1,32 @@
+//
+//  RealmTrip.swift
+//  RideHitch
+//
+//  Created by Andrew Roach on 1/17/17.
+//  Copyright © 2017 Andrew Roach. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class RealmTrip: Object {
+
+    dynamic var _tripID = 0
+    dynamic var _creationDate = 0.0
+    dynamic var _creatorUserID = ""
+    dynamic var _destinationDate = 0.0
+    dynamic var _destinationLatitude = 0.0
+    dynamic var _destinationLongitude = 0.0
+    dynamic var _destinationName = ""
+    dynamic var _destinationGeohash = ""
+
+    dynamic var _isDriver =  false
+    dynamic var _isMatched = false
+    dynamic var _originDate = 0.0
+    dynamic var _originLatitude = 0.0
+    dynamic var _originLongitude = 0.0
+    dynamic var _originName = ""
+    dynamic var _originGeohash = ""
+
+    
+}
